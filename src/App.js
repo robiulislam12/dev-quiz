e11347a1ec0380c0808363de901cb8fe23a8bb5a
robiulislam2520 <robiul215565@gmail.com>
2022-10-10 22:32:15 +0600

    React router dom and react icons setup aand tailwind css use

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <div className="App">
-      <h2>Hello world</h2>
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
